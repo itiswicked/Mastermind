632749506a34442af1bb60074f4d4da18a1ee87a
Nathan Bourke <nathanrbourke@gmail.com>
2015-03-06 16:04:17 -0500

    Create Board class. Complete CodeSelector, Board testing

--- mastermind.rb
+++ mastermind.rb
@@ -1,28 +1,84 @@
 # Based on Parker Mastermind cir. 1993
+class Player;end
 
-class Game
 
-  attr_accessor :answer
-  attr_reader :colors
+class Board
+  attr_accessor :decode_board, :display_board, :hint_board
 
   def initialize
-  	@code = []
-    @colors = ["Red","Blue","Green","Yellow",
-               "Brown","Orange","Black","White"]
+    # The playing 'surface' in which the user makes guesses. One row per turn.
+    @decode_board = [["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"],
+                     ["--","--","--","--"]]
+    # A place for the program to give feedback on the user guess.           
+    @hint_board = [["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"],
+                   ["--","--","--","--"]]
   end
 
-  def push_to_code
+  def board_titles
+    "Turn       Hint Board            Decode Board".center(20)
+  end
+# upon each iteration, returns boars and hint row next to each other.
+  def show_boards
+    decode_board.each_with_index do |decode_row, index|
+      puts "#{index}      " + "#{hint_board[index].join(" | ")}   " "   #{decode_row.join(" | ")}"
+    end
+  end
+# turn is index(row) of decode_board to be replaced,
+# update is array to replace the one there.
+  def update_decode_board(turn, update)  
+    decode_board[turn] = update
+  end
+  
+  def update_hint_board(turn,update)
+    hint_board[turn] = update
+  end
+
+end
+
+class CodeSelector
+  attr_accessor :answer, :colors
+
+  def initialize
+  	@answer = []
+    @colors = ["Rd","Bl","Gr","Yw",
+               "Br","Or","Bk","Wh"]
+  end
+
+  def select_answer
     4.times do
-      code << colors.random_select
+      answer << colors.random_select
+      delete_color
     end
   end
 
-  def random_select
-    self[rand(self.size)]
+
+  def delete_color
+    colors.delete(answer.last)
   end
-
-  def delete_color(color)
-    colors.delete(color)
-
 end
+
+class Array
+  def random_select
+    self.fetch(rand(self.size) - 1)
+  end
+end
+
+p = Player.new
+
 
