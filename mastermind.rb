9e4c79a76832e4656702a23856639da8baa0c790
Nathan Bourke <nathanrbourke@gmail.com>
2015-03-23 20:56:59 -0400

    Create Player class and control flow. First working version.

--- mastermind.rb
+++ mastermind.rb
@@ -1,8 +1,64 @@
-class Player;end
+class Player
+  attr_accessor :user_input, :turn, :hint, :code, :board
+
+  def initialize
+    @user_input = Array.new
+    @turn = 0
+    @hint = []
+    @board = Board.new
+    @code = CodeSelector.new
+  end
+
+# Do all user_input elements equal an element in code.colors array?
+  def valid_input?
+    if user_input.length == 4
+      user_input.all? do |input_color|
+        code.colors.any? do |code_color|
+          code_color == input_color
+        end
+      end
+    end
+  end
+
+  # For every answer element and its index that equals a user_input element
+  # and its input index, push "Rd" to hint array. 
+  # For every answer element that equals a user input element, but not
+  # neccesarily its index, push "Wh" to the hint array.
+  def create_hint
+    user_input.each_with_index do |color, index|
+      code.answer.each_with_index do |code_color, code_index|
+        if color == code_color && index == code_index
+          hint << "Rd"
+        elsif color == code_color
+          hint << "Wh"
+        end
+      end
+    end
+  end
+
+  def fill_in_hint
+    while hint.length < 4
+      hint << "--"
+    end
+  end
+
+  def win?
+    user_input == code.answer
+  end
+
+  def lose?
+    turn > 9
+  end
+
+  def end_turn
+    @turn = @turn + 1
+  end
+
+end
 
 
 class Board
-  attr_accessor :decode_table, :display_board, :hint_table
+  attr_accessor :decode_table, :hint_table
 
   def initialize
     # The playing 'surface' in which the user makes guesses. One row per turn.
@@ -29,18 +85,18 @@ class Board
                    ["--","--","--","--"]]
   end
 
-  def board_titles
+  def show_titles
     "Turn       Hint Table            Decode Table".center(20)
   end
-# upon each iteration, returns boars and hint row inline.
-  def show_boards
+# upon each iteration, returns decode and hint row inline.
+  def show
     decode_table.each_with_index do |decode_row, index|
       puts "#{index}      " + "#{hint_table[index].join(" | ")}   " "   #{decode_row.join(" | ")}"
     end
   end
 # turn is index(row) of decode_table to be replaced,
-# update is array to replace the one there.
-  def update_decode_table(turn, update)  
+# update is 4 element array to replace the one there.
+  def update_decode_table(turn,update)  
     decode_table[turn] = update
   end
   
@@ -51,24 +107,26 @@ class Board
 end
 
 class CodeSelector
-  attr_accessor :answer, :colors
+  attr_accessor :answer, :colors, :color_choices
 
   def initialize
-  	@answer = []
+    @answer = []
     @colors = ["Rd","Bl","Gr","Yw",
                "Br","Or","Bk","Wh"]
+    @color_choices = ["Rd","Bl","Gr","Yw",
+                      "Br","Or","Bk","Wh"]
   end
 
   def select_answer
     4.times do
-      answer << colors.random_select
+      answer << color_choices.random_select
       delete_color
     end
   end
 
 
   def delete_color
-    colors.delete(answer.last)
+    color_choices.delete(answer.last)
   end
 end
 
@@ -78,5 +136,35 @@ class Array
   end
 end
 
+p = Player.new
 
+puts "\nWelcome to Mastermind!
+\nPlease read the rules at http://en.wikipedia.org/wiki/Mastermind_(board_game).
+\nThis program is based on the 1993 edition of Parker Mastermind. The variation
+\nrules can also be viewed on the wikipedia page. To make a guess, simply type
+\nthe colors in seperated by spaces. Read the readme for color abbreviations."
 
+p.code.select_answer
+
+loop do
+  puts p.board.show_titles
+  p.board.show
+  puts "Please enter your guess:"
+  p.user_input = gets.chomp.split.map(&:capitalize)
+  if p.valid_input?
+    if p.win?
+      puts "You won!"
+    elsif p.lose?
+      puts "You lost!"
+    else
+      p.create_hint
+      p.fill_in_hint
+      p.board.update_hint_table(p.turn,p.hint)
+      p.board.update_decode_table(p.turn,p.user_input)
+      p.end_turn
+    end
+  else
+    puts "Not a valid input!"
+  end
+  p.hint = []
+end
