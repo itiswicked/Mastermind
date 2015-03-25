88b1056175503c5f3119e2ae60d430676dfe5f8e
Nathan Bourke <nathanrbourke@gmail.com>
2015-03-25 15:46:03 -0400

    Complete first refactor. Update/fix issues with readme.

--- README.md
+++ README.md
@@ -1,32 +1,33 @@
 Mastermind
-----------
+==========
 Recreating the game Mastermind in Ruby
 
 Based on Parker Mastermind cir. 1993
 
-Please see the Wikipedia entry on [Mastermind](http://example.com) for variations.
+Please see the Wikipedia entry on [Mastermind](http://en.wikipedia.org/wiki/Mastermind_(board_game)) for variations.
 
 #Rules
 
-The computer selects four colors out of 8 available and places them in a specific order
+The computer selects four colors out of 8 available and places them in a specific order.
 
 The Colors are:
-  *Red 'Rd'
-  *Blue 'Bl'
-  *Green 'Gr'
-  *Yellow 'Yw'
-  *Brown 'Br'
-  *Black 'Bk'
-  *White 'Wh'
-  *Orange 'Or'
 
-It is your task to guess those colors within 10 turns. At the beginning of each turn, select the colors you would like to guess by typing them into the comand prompt using the shorthand above, seperated by commas.
+  * Red 'Rd'
+  * Blue 'Bl'
+  * Green 'Gr'
+  * Yellow 'Yw'
+  * Brown 'Br'
+  * Black 'Bk'
+  * White 'Wh'
+  * Orange 'Or'
+
+It is your task to guess those colors within 10 turns. At the beginning of each turn, select the colors you would like to guess by typing them into the command prompt using the shorthand above, seperated by spaces.
 
 Example:
 
-$ Br, Rd, Bk, Wh
+$ Br Rd Bk Wh
 
-These guesses will show up in the Decode Table.Remember, the color order counts, as you must guess a correct match to win!
+These guesses will show up in the Decode Table. Remember: the order of your guess counts! No duplicate colors are allowed in this version of the game.
 
 At the beginning of each turn you will recieve feedback on your previous guess, 
 displayed in the Hint Table.
@@ -34,6 +35,6 @@ displayed in the Hint Table.
 Hint Table:
 
   * White ('Wh') hint cells filled in by the program indicates that you have a correct color, but not in the right position
-  * Red ('Rd') hint cells filled in by the program indicates that you have a correct color in the correct position
+  * Bk ('Bk') hint cells filled in by the program indicates that you have a correct color in the correct position
 
-Obviously the game 
+Happy hunting!
--- mastermind.rb
+++ mastermind.rb
@@ -1,140 +1,163 @@
 class Player
-  attr_accessor :user_input, :turn, :hint, :code, :board
+  attr_accessor :input, :turn, :decode, :answer, :hint, :display, :colors
 
   def initialize
-    @user_input = Array.new
+    @input = []
     @turn = 0
-    @hint = []
-    @board = Board.new
-    @code = CodeSelector.new
+    @decode = Decode.new
+    @answer = Answer.new
+    @hint = Hint.new
+    @display = Display.new
+    @colors = ["Rd","Bl","Gr","Yw",
+                "Br","Or","Bk","Wh"]  
   end
 
-# Do all user_input elements equal an element in code.colors array?
+  # Do all user_input elements equal an element in colors array?
   def valid_input?
-    if user_input.length == 4
-      user_input.all? do |input_color|
-        code.colors.any? do |code_color|
+    if input.length == 4
+      input.all? do |input_color|
+        colors.any? do |code_color|
           code_color == input_color
         end
       end
     end
   end
 
-  # For every answer element and its index that equals a user_input element
-  # and its input index, push "Rd" to hint array. 
-  # For every answer element that equals a user input element, but not
-  # neccesarily its index, push "Wh" to the hint array.
-  def create_hint
-    user_input.each_with_index do |color, index|
-      code.answer.each_with_index do |code_color, code_index|
-        if color == code_color && index == code_index
-          hint << "Rd"
-        elsif color == code_color
-          hint << "Wh"
+  def win?
+    input == answer.answer
+  end
+
+  def lose?
+    turn > 8
+  end
+
+  def end_turn
+    @turn += 1
+  end
+
+end
+
+class Display < Player
+  # A class specifically for handling merging and displaying the two game tables
+  def initialize;end
+
+  def show_titles
+    "Turn       Hint Table            Decode Table".center(20)
+  end
+
+  # REWRITE SO NOT SO MESSY
+  def show_tables(hint_table,decode_table)
+    decode_table.each_with_index do |decode_row, index|
+      puts "#{index}      " + "#{hint_table[index].join(" | ")}   " "   #{decode_row.join(" | ")}"
+    end
+  end
+
+end
+
+class Answer
+  attr_accessor :answer, :colors
+
+  def initialize
+    @answer = []
+    @colors = ["Rd","Bl","Gr","Yw",
+               "Br","Or","Bk","Wh"]   
+  end
+
+  def create
+    4.times do
+      answer << colors.random_select
+      delete_color
+    end
+  end
+
+  private
+
+  def delete_color
+    colors.delete(answer.last)
+  end
+
+end
+
+class Hint
+  attr_accessor :table, :current_hint
+
+  def initialize
+    @table = [["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"]]
+    @current_hint = []
+  end
+
+  def create(input,answer) 
+    input.each_with_index do |input_color, index|
+      answer.each_with_index do |answer_color, answer_index|
+        # if color is present and in the right place
+        if input_color == answer_color && index == answer_index 
+          current_hint << "Bk"
+        # if color is simply present
+        elsif input_color == answer_color
+          current_hint << "Wh"
         end
       end
     end
   end
 
-  def fill_in_hint
-    while hint.length < 4
-      hint << "--"
+  # this method fills in current_hint with the appropriate number of "--"
+  # after Bk's Wh's have been populated into it.
+  def fill_in
+    while current_hint.length < 4
+      current_hint << "--"
     end
   end
 
-  def win?
-    user_input == code.answer
+  def update(turn)
+    table[turn] = current_hint.sort
   end
 
-  def lose?
-    turn > 9
-  end
-
-  def end_turn
-    @turn = @turn + 1
+  def delete 
+    current_hint = []
   end
 
 end
 
-
-class Board
-  attr_accessor :decode_table, :hint_table
-
-  def initialize
-    # The playing 'surface' in which the user makes guesses. One row per turn.
-    @decode_table = [["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"],
-                     ["--","--","--","--"]]
-    # A place for the program to give feedback on the user guess.           
-    @hint_table = [["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"],
-                   ["--","--","--","--"]]
-  end
-
-  def show_titles
-    "Turn       Hint Table            Decode Table".center(20)
-  end
-# upon each iteration, returns decode and hint row inline.
-  def show
-    decode_table.each_with_index do |decode_row, index|
-      puts "#{index}      " + "#{hint_table[index].join(" | ")}   " "   #{decode_row.join(" | ")}"
-    end
-  end
-# turn is index(row) of decode_table to be replaced,
-# update is 4 element array to replace the one there.
-  def update_decode_table(turn,update)  
-    decode_table[turn] = update
-  end
-  
-  def update_hint_table(turn,update)
-    hint_table[turn] = update
-  end
-
-end
-
-class CodeSelector
-  attr_accessor :answer, :colors, :color_choices
+class Decode
+  attr_accessor  :table
 
   def initialize
-    @answer = []
-    @colors = ["Rd","Bl","Gr","Yw",
-               "Br","Or","Bk","Wh"]
-    @color_choices = ["Rd","Bl","Gr","Yw",
-                      "Br","Or","Bk","Wh"]
+    @table = [["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"],
+              ["--","--","--","--"]]
   end
 
-  def select_answer
-    4.times do
-      answer << color_choices.random_select
-      delete_color
-    end
+  # turn is index(row) of decode_table to be replaced,
+  # update is 4 element array to replace the one there.
+  def update(colors,turn)
+    table[turn] = colors
   end
 
-
-  def delete_color
-    color_choices.delete(answer.last)
-  end
 end
 
 class Array
   def random_select
     self.fetch(rand(self.size) - 1)
   end
+
 end
+
 
 p = Player.new
 
@@ -144,27 +167,30 @@ puts "\nWelcome to Mastermind!
 \nrules can also be viewed on the wikipedia page. To make a guess, simply type
 \nthe colors in seperated by spaces. Read the readme for color abbreviations."
 
-p.code.select_answer
+p.answer.create
 
 loop do
-  puts p.board.show_titles
-  p.board.show
+  puts p.display.show_titles
+  p.display.show_tables(p.hint.table,p.decode.table)
   puts "Please enter your guess:"
-  p.user_input = gets.chomp.split.map(&:capitalize)
+  p.input = gets.chomp.split.map(&:capitalize)
   if p.valid_input?
     if p.win?
-      puts "You won!"
+      puts "You won!  Answer: #{p.answer.answer.join(' | ')}"
+      exit
     elsif p.lose?
-      puts "You lost!"
+      puts "You lost! Answer: #{p.answer.answer.join(' | ')}"
+      exit
     else
-      p.create_hint
-      p.fill_in_hint
-      p.board.update_hint_table(p.turn,p.hint)
-      p.board.update_decode_table(p.turn,p.user_input)
+      p.hint.create(p.input,p.answer.answer)
+      p.hint.fill_in
+      p.hint.update(p.turn)
+      p.decode.update(p.input,p.turn)
+      p.hint.current_hint = []
       p.end_turn
     end
   else
     puts "Not a valid input!"
   end
-  p.hint = []
 end
+
