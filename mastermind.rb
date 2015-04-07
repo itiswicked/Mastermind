7c9235120c7dd401829175054caea345d1d88245
Nathan Bourke <nathanrbourke@gmail.com>
2015-04-07 12:12:01 -0400

    Clean and temporarily disable computer-as-player functions.

--- mastermind.rb
+++ mastermind.rb
@@ -26,25 +26,16 @@ class Player
 end
 
 class Display
-  def titles
-    "Turn       Hint Table            Decode Table".center(20)
-  end
 
   def tables(hint_table,decode_table)
-    decode_table.reduce(0) do |turn, decode_row|
-      puts "#{turn}      " + "#{hint_table[turn].join(" | ")}   " "   #{decode_row.join(" | ")}"
-      turn + 1
+    puts titles
+    decode_table.each_with_index do |decode_row, turn|
+      puts "#{turn}           #{hint_table[turn].join(" | ")}        #{decode_row.join(" | ")}"
     end
   end
 
-end
-
-class Answer
-  attr_accessor :answer, :colors
-
-  def initialize
-    @colors = ["Rd","Bl","Gr","Yw","Br","Or","Bk","Wh"]
-    @answer = []
+  def titles
+    "Turn           Hint Table              Decode Table"
   end
 
 end
@@ -66,36 +57,37 @@ class Hint
     @current_hint = []
   end
 
-  def create(input,answer)
-    input.reduce(0) do |position, input_color|
-      answer.reduce(0) do |answer_position, answer_color|
-        # if color is present and in the right place
-        if input_color == answer_color && position == answer_position
+
+  def create(input,answer,turn)
+    input.each_with_index do |input_color,input_index|
+      answer.each_with_index do |answer_color,answer_index|
+        if input_color == answer_color && input_index == answer_index
           current_hint << "Bk"
-        # if color is simply present
         elsif input_color == answer_color
           current_hint << "Wh"
         end
-        answer_position + 1
       end
-      position + 1
     end
+    append
+    current_hint.sort
+    update(turn)
+    delete
   end
 
-  # this method fills in current_hint with the appropriate number of "--"
-  # after Bk's and Wh's have been populated into it.
-  def fill_in
-    while current_hint.length < 4
-      current_hint << "--"
-    end
-  end
+  private
 
   def update(turn)
-    table[turn] = current_hint.sort
+    table[turn] = current_hint
   end
 
   def delete
-    current_hint = []
+    @current_hint = []
+  end
+
+  def append
+    while current_hint.length < 4
+      current_hint << "--"
+    end
   end
 
 end
@@ -124,48 +116,52 @@ class Decode
 
 end
 
-class Computer < Player
-  attr_accessor :guess
+class Game
+  attr_accessor :player, :decode, :hint, :display, :colors, :answer
 
   def initialize
-    @guess = []
-  end
-
-end
-
-class Game
-  attr_accessor :player, :decode, :answer, :hint, :display, :computer, :user_role
-
-  def initialize(user_role)
-    @player = Player.new
-    @decode = Decode.new
-    @answer = Answer.new
-    @hint = Hint.new
+    @player  = Player.new
+    @decode  = Decode.new
+    @hint    = Hint.new
     @display = Display.new
-    @computer = Computer.new
-    @user_role = user_role
+    @colors  = ["Rd","Bl","Gr","Yw","Br","Or","Bk","Wh"]
+    @answer  = ["Gr","Yw","Br","Or"]
   end
 
-  def select_user_path
-    case user_role
-    when "Self"
-      play_as_codebreaker
-    when "Comp"
-      play_as_codemaker
-    end
-  end
+  ##########################################################################
+  ### Methods commented out are for later expansion to play as codemaker ###
+  ##########################################################################
 
-  private
+  #def start
+  #  loop do
+  #    puts "Please choose Self or Comp to determine your role:"
+  #    user_role = gets.chomp.capitalize
+  #    if user_role == "Self" || "Comp"
+  #      select_user_path
+  #      break
+  #    else
+  #       puts "Invalid input, try again."
+  #    end
+  #  end
+  #end
+
+  #def select_user_path
+  #  case user_role
+  #  when "Self"
+  #    play_as_codebreaker
+  #  when "Comp"
+  #    play_as_codemaker
+  #  end
+  #end
 
   def play_as_codebreaker
-    answer.answer = answer.colors.sample(4)
+    # answer = colors.sample(4)
     loop do
-      puts display.titles
       display.tables(hint.table,decode.table)
-      puts "Please enter your guess #{player.colors.join(' | ')}:"
+      puts "Please enter your guess #{colors.join(' | ')}:"
       player.input = gets.split.map(&:capitalize)
       if player.valid_input?
-        if player.win?(answer.answer)
+        if player.win?(answer)
           puts "You won! #{display_answer}"
           exit
         elsif player.lose?
@@ -180,51 +176,38 @@ class Game
     end
   end
 
-  def play_as_codemaker
-    puts "Please select code colors:"
-    answer.answer = gets.split.map(&:capitalize)
-    loop do
-      player.input = player.colors.sample(4)
-      puts display.titles
-      display.tables(hint.table,decode.table)
-      if player.win?(answer.answer)
-        puts "Computer Won!"
-        exit
-      elsif player.lose?
-        puts "Computer Lost!"
-        exit
-      else
-        complete_turn
-      end
-    end
-  end
+  #def play_as_codemaker
+  #  puts "Please select code colors:"
+  #  answer = gets.split.map(&:capitalize)
+  #  loop do
+  #    player.input = colors.sample(4)
+  #    display.tables(hint.table,decode.table)
+  #    if player.win?(answer)
+  #      puts "Computer Won!"
+  #      exit
+  #    elsif player.lose?
+  #      puts "Computer Lost!"
+  #      exit
+  #    else
+  #      complete_turn
+  #    end
+  #  end
+  #end
 
   def display_answer
-    "Answer: #{answer.answer.join(' | ')}"
+    "Answer: #{answer.join(' | ')}"
   end
 
   def complete_turn
-    hint.create(player.input,answer.answer)
-    hint.fill_in
-    hint.update(player.turn)
+    hint.create(player.input,answer,player.turn)
     decode.update(player.input,player.turn)
-    hint.current_hint = []
     player.end_turn
   end
 
 end
 
-puts "\nWelcome to Mastermind! Refer to the readme for the rules!\n\nYou can choose to solve the code yourself(Self), or set the code and watch the computer attempt to solve it(Comp)."
+puts "\nWelcome to Mastermind! Refer to the readme for the rules!"
+# puts "\n\nYou can choose to solve the code yourself(Self), or set the code and watch the computer attempt to solve it(Comp)."
 
-loop do
-  puts "Please choose Self or Comp to determine your role:"
-  $user_role = gets.chomp.capitalize
-  if $user_role == "Self" || "Comp"
-    break
-  else
-     puts "Invalid input, try again."
-  end
-end
-
-game = Game.new($user_role)
-game.select_user_path
+game = Game.new
+game.play_as_codebreaker
